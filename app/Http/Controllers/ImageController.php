5318d78d0a04ca148489d970f905fabdc8fb9fe5
Onyeka Ijeh <onyekaijeh@yahoo.com>
2018-02-05 01:15:09 +0100

    fix image uploading

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -7,25 +7,25 @@ use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
-    public function upload(Request $request){
-
-        $images = [];
-
-        foreach($request->all() as $file){
-
-          if(is_file($file)){
-
-            $file_name = time() . '-' . $file->getClientOriginalName();
-            
-            $file->storeAs('public/uploads', $file_name);
-
-            $file_path = Storage::url('uploads/'.$file_name);
-
-            $images[] = $file_path;
-          }
-  
-        }
-        return response()->json($images, 200);
-      }
-  
+	public function upload(Request $request){
+		
+		$images = [];
+		
+		foreach($request->all() as $file){
+			
+			if(is_file($file)){
+				
+				$folder_name = '/news';
+				
+				$file_path = Storage::disk('public_uploads')->put($folder_name, $file);
+				
+				$file_path = '/uploads/'.$file_path;
+				
+				$images[] = $file_path;
+			}
+			
+		}
+		return response()->json($images, 200);
+	}
+	
 }
